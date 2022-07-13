cce9febce7909763073b982cf3aa9bd3f2b0c5ec
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-13 15:31:35 -0500

    :tada: Begin

---
+++ server.ts
@@ -0,0 +1,11 @@
+import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+const app = new Application();
+
+const SPOTIFY_PLAYLIST = `https://open.spotify.com/playlist/0d1TPvnEGO3mGsLupY1teE?si=98a3b49f7e9b45df`;
+
+app.use((ctx) => {
+  ctx.response.redirect(SPOTIFY_PLAYLIST);
+});
+
+app.listen({ port: 8000 });
